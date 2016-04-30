d39237ddaad2c54e429564e1e5896ddb3c1e5a0c
Jaiveer Singh <jsboygenius@gmail.com>
2016-04-29 20:54:56 -0700

    END OF SEASON
    It's been a gr9 season guys

---
+++ 2015-16 Nothing But Net/Extras/Saved Routines (RobotC)/john.h
@@ -0,0 +1,10 @@
+//Automatically generated code via VRC 5327B NbN Simulation
+void John()
+{
+  //Turn
+  ABase(0, 0, 127, 2);
+  //Forward
+  ABase(127, 0, 0, 6);
+  //Left
+  ABase(0, -127, 0, 3);
+}

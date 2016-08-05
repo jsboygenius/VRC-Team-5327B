de45ab18690a266aac0c864666308f3beea10b17
Jaiveer Singh <jsboygenius@gmail.com>
2016-08-05 03:24:15 -0700

    Full Joystick Control

--- 2016-17 Starstruck/Competition/1_Driver_Control.h
+++ 2016-17 Starstruck/Competition/1_Driver_Control.h
@@ -1,6 +1,6 @@
 //This header file contains the necessary functions to be run throughout the competition
 
-
+int lastBaseState = 0;
 /*
 Function governing the movement of the base. Takes 4 parameters:
 @X_comp : How much we want to strafe
@@ -8,7 +8,7 @@ Function governing the movement of the base. Takes 4 parameters:
 @rot_comp : How much we want to rotate the robot CW/CCW
 @duration : How long to drive
 */
-void BaseControl(int X_comp, int Y_comp, int rot_comp, int slow = 0, int duration = LOOP_DELAY)
+void BaseControl(int X_comp, int Y_comp, int rot_comp, int slow = 0, int directionToggle = 0)
 {
 	/*
 	Assuming the following control scheme:
@@ -21,6 +21,12 @@ void BaseControl(int X_comp, int Y_comp, int rot_comp, int slow = 0, int duratio
 	SetMotor(LFBase, Y_comp + X_comp + rot_comp);
 	SetMotor(RBBase, Y_comp + X_comp - rot_comp);
 	SetMotor(LBBase, Y_comp - X_comp + rot_comp);
+	if(directionToggle != lastBaseState)
+	{
+		robotDirection *= -1;
+		SensorValue[DirectionLED] = abs(SensorValue[DirectionLED] - 1);
+	}
+	lastBaseState = directionToggle;
 }
 
 void LaunchRM(int speed)
@@ -43,35 +49,81 @@ void LaunchM(int speed)
 	LaunchLM(speed);
 }
 
-/*
-Function governing the launcher firing. Takes 2 parameters:
-@direction: direction to adjust launcher, 1 = forward, 0 = backward
-*/
-void LaunchControl(int starModifier, int launchTrigger, int manualAdjust = 0, int lockAdjust = 0)
+void autoDump()
 {
+	//Ensure lock is unlocked
+	SensorValue[Lock] = 0;
+	//Dump until sensor is triggered
+	while(SensorValue[RopeTaut] == 0)
+	{
+		LaunchM(127);
+	}
+	//Release until forklift is back down
+	while(SensorValue[LaunchDownR] == 0 || SensorValue[LaunchDownL] == 0)
+	{
+		LaunchRM( (SensorValue[LaunchDownR] == 0) ? -127 : 0 );
+		LaunchLM( (SensorValue[LaunchDownL] == 0) ? -127 : 0 );
+	}
+	LaunchM(0);
+}
 
-	switch(starModifier)
+int* determinePullback(int* inputValues, int len)
+{
+	len = abs(len);
+	int bestSoFar = 0;
+	for(int i = 0; i < len; i++)
 	{
-	case 1:
-		//pullback for 1 star
-		break;
-	case 10:
-		//pullback for 2 stars
-		break;
-	case 100:
-		//pullback for 3 stars
-		break;
+		bestSoFar = inputValues[i] > bestSoFar ? inputValues[i] : bestSoFar;
 	}
-	if(LaunchTrigger == 1 && starModifier != 0)
+	int returnArray[2] = {PULLBACK_CONSTANTS[bestSoFar], HOLD_CONSTANTS[bestSoFar]};
+	return returnArray;
+}
+
+void autoLaunchReset()
+{
+	//Ensure lock is locked
+	SensorValue[Lock] = 1;
+	//Lift until forklift barely in air
+	while(SensorValue[LaunchDownR] == 1 || SensorValue[LaunchDownL] == 1)
 	{
-		//Ready to launch!
-		//Initiate launch sequence
+		LaunchRM( (SensorValue[LaunchDownR] == 1) ? 127 : 25 );
+		LaunchLM( (SensorValue[LaunchDownL] == 1) ? 127 : 25 );
 	}
-	LaunchM(127 * manualAdjust);
-	if(lockAdjust != 0)
+}
+
+void autoLaunchPullback(int* inputs)
+{
+	//Ensure lock is locked (again)
+	SensorValue[Lock] = 1;
+	//Pullback for specified amount, then hold at specified power
+	LaunchM(127);
+	wait1Msec(inputs[0]);
+	LaunchM(inputs[1]);
+}
+
+void autoLift()
+{
+	//Ensure that lock is unlocked
+	SensorValue[Lock] = 0;
+	//Engage transmission
+	SensorValue[Transmission] = 1;
+	transmissionActive = true;
+	//Begin raising lift
+	waitUntil(autoLiftBtn == 0);
+	while(SensorValue[LiftPot] < MAX_LIFT)
 	{
-		SensorValue[Lock] = sgn(1 + lockAdjust);
+		LaunchM(127);
 	}
+	LaunchM(25);
+	waitUntil(autoLiftBtn == 1);
+	while(SensorValue[LiftPot] > MIN_LIFT)
+	{
+		LaunchM(-127);
+	}
+	LaunchM(LIFT_DOWN_HOLD);
+	//Lock lift down
+	SensorValue[Lock] = 1;
+	LaunchM(0);
 }
 
 //This header file contains the necessary tasks to run the major components of the robot simultaneously during Driver Control
@@ -83,30 +135,83 @@ task Drive()
 {
 	while(true)
 	{
-		BaseControl(X_Joy * robotDirection, Y_Joy * robotDirection, rot_Joy, toggleSlowBtn);
-		if(directionToggleBtn == 1)
+		BaseControl(X_Joy * robotDirection, Y_Joy * robotDirection, rot_Joy, toggleSlowBtn, directionToggleBtn);
+		EndTimeSlice();
+	}
+}
+
+/*
+Task that governs all code related to the launcher
+*/
+task Gearbox()
+{
+	while(true)
+	{
+		//First, determine whether we are in debug or not
+		if(debugModeEnabled)
 		{
-			robotDirection *= -1;
-			SensorValue[DirectionLED] = abs(SensorValue[DirectionLED] - 1);
-			waitUntil(directionToggleBtn != 1);
+			if(LiftTransmissionBtn == 1) //Check lifting first
+			{
+				SensorValue[Transmission] = 1 - SensorValue[Transmission];
+				transmissionActive = ! transmissionActive;
+				playSound(transmissionActive ? soundUpwardTones : soundDownwardTones);
+				waitUntil(LiftTransmissionBtn == 0);
+			}
+			if(transmissionActive) //If we are lifting, we can't also use debug on launcher
+			{
+				LaunchM((LiftUpBtn - LiftDownBtn) * 127);
+			}
+			else //If we are not lifting, then we can debug launcher
+			{
+				LaunchRM(127 * (LaunchUpBtn - LaunchDownBtn) * sgn(LaunchRightBtn - LaunchLeftBtn + 1));
+				LaunchLM(127 * (LaunchUpBtn - LaunchDownBtn) * sgn(LaunchLeftBtn - LaunchRightBtn + 1));
+				if(LaunchLockBtn == 1)
+				{
+					SensorValue[Lock] = 1;
+				}
+				else if(LaunchUnlockBtn == 1)
+				{
+					SensorValue[Lock] = 0;
+				}
+			}
+		}
+		else //Not in debug, so normal functionality
+		{
+			if(autoLiftBtn == 1) //Again, check lift first
+			{
+				autoLift();
+			}
+			else if(DumpModifierBtn == 1) //Next, check if we want to dump
+			{
+				autoDump();
+			}
+			else if(LaunchModifierBtn == 1 && Modifier1StarBtn + Modifier2StarBtn + Modifier3StarBtn + ModifierCubeBtn > 0) //Lastly, check if we want to launch
+			{
+				waitUntil(LaunchModifierBtn == 0);
+				int launchModifiers[4] = {Modifier1StarBtn * 1, Modifier2StarBtn * 2, Modifier3StarBtn * 3, ModifierCubeBtn * 4};
+				autoLaunchReset();
+				autoLaunchPullback(determinePullback(launchModifiers, 4));
+				waitUntil(LaunchModifierBtn == 1);
+				autoDump();
+			}
 		}
 		EndTimeSlice();
 	}
 }
 
-
-/*
-Task that governs all code related to the launcher
-*/
-task Launch()
+task Debug()
 {
 	while(true)
 	{
-		LaunchControl(Modifier1Star + Modifier2Star * 10 + Modifier3Star * 100, LaunchTrigger, LaunchUpBtn - LaunchDwBtn, LaunchUnlockBtn - LaunchLockBtn);
+		if(debugBtn == 1)
+		{
+			debugModeEnabled = !debugModeEnabled;
+			playSound(debugModeEnabled ? soundUpwardTones : soundDownwardTones);
+			waitUntil(debugBtn == 0);
+		}
 		EndTimeSlice();
 	}
 }
-
 
 /*
 Task that governs all code related to emergency stopping of the robot tasks and other code
@@ -118,7 +223,8 @@ task EmergencyOverride()
 		if(overrideBtn == 1) //If override triggered by driver, stop only the tasks that need to be stopped (do not stop task Main()!!!)
 		{
 			stopTask(Drive);
-			stopTask(Launch);
+			stopTask(Gearbox);
+			stopTask(Debug);
 			BaseControl(0, 0, 0);
 			for(int i = 1; i <= 10; i++) //Reset all motors in motor array to 0
 			{
@@ -126,7 +232,8 @@ task EmergencyOverride()
 			}
 			wait1Msec(2000); //Short 2 second delay to ensure everything has been reset
 			startTask(Drive);
-			startTask(Launch);
+			startTask(Gearbox);
+			startTask(Debug);
 		}
 		wait1Msec(1000); //Check the button only once per second, to prevent accidental rapid reset that can lead to errors
 		//in CPU timeslices
--- 2016-17 Starstruck/Competition/4_CONSTANTS.h
+++ 2016-17 Starstruck/Competition/4_CONSTANTS.h
@@ -1,11 +1,25 @@
 //This header file contains all of the constants referenced throughout the driver competition code.
 //EDIT WITH CAUTION!
 
-//General Constants:
-const int LOOP_DELAY = 10; //How long between each refresh of the while loops governing robot code, used because we do not need 1000 refreshes per second
-
 //Base Constants:
 const int SLOW_COEFFICIENT = 0.75; //How fast to turn the robot while on slow mode
 
 //Launcher Constants:
-const int LAUNCHER_BACK_SPEED = 127; //How fast the launcher is adjusted on Slow setting
+const int PULLBACK_CONSTANTS[] = {
+	1250, //1 star, 3.5 feet
+	1500, //2 stars, 3.5 feet
+	2000, //3 stars, 3.5 feet
+	1500  //1 cube, 5 feet
+};
+
+const int HOLD_CONSTANTS[] = {
+	25,
+	25,
+	35,
+	25
+};
+
+//Lift Constants:
+const int MAX_LIFT = 3500;
+const int MIN_LIFT = 400;
+const int LIFT_DOWN_HOLD = 50;
